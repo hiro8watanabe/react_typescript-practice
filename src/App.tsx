cb7a682d35bbd2f831ad5a86f14a2d9bce4c61f1
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-12 14:35:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Practice1 } from "./practices/Practice1";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Practice1 />
+    </div>
+  );
+};

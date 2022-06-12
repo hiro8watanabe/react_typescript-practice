e92c23523066f9f4e6dc2c1cd42d2d6c2db99828
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-12 14:48:16 +0900

    Practice2 返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 import "./styles.css";
 
 export const App = () => {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 };
